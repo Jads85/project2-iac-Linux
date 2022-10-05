5c247da523a23ecc6e68c929ea9a1866c9e87c40
Jadson França <63564173+Jads85@users.noreply.github.com>
2022-10-05 17:36:31 -0300

    Projeto 2  IAC
    Um script que automatiza a criação de um servidor web através do Linux.

---
+++ iac2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo"Update Server ..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+
+echo "Download and copy app ..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
